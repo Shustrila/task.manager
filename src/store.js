54509940add1e6755d559158b94022ecbfa46827
Александр Махалов <aleksandrmahalov@MacBook-Aleksandr.local>
2018-11-29 23:47:47 +0300

    add

--- src/store.js
+++ src/store.js
@@ -5,12 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    task: [
+      {
+        name: 'scsc',
+        desc: 'scscsc'
+      }
+    ]
   },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+  mutations: {},
+  actions: {},
+  getters: {}
 })
